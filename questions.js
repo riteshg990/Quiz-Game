184d4d216549cdddef3ec070be5288333efe95a6
riteshg990 <42331286+riteshg990@users.noreply.github.com>
2021-03-26 00:46:21 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,114 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does TCp stand for?",
+    answer: "Transmission control protocol",
+    options: [
+      "Transmission control protocol",
+      "Transmission Sheet",
+      "trns",
+      "transmission common practice"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What is the 16-bit compiler allowable range for integer constants??",
+    answer: "-32768 to 32767",
+    options: [
+      "-3.4e38 to 3.4e38",
+      "-32767 to 32768",
+      "5676674",
+      "4788857"
+    ]
+  },
+   {
+    numb: 7,
+    question: "What is required in each C program?",
+    answer: " The program must have at least one function",
+    options: [
+      " The program must have at least one function",
+      "input",
+      "output",
+      "Null"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is a lint?",
+    answer: "C compiler",
+    options: [
+      "Interactive debugger",
+      "C compiler",
+      "Analyzing tool",
+      "C interpreter"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which one of the following is a loop construct that will always be executed once?",
+    answer: "do while",
+    options: [
+      " if",
+      "do while",
+      "for",
+      "while"
+    ]
+  },
+   {
+    numb: 10,
+    question: "Directives are translated by the?",
+    answer: "Pre-processor",
+    options: [
+      "Compiler",
+      "Pre-processor",
+      "Linker",
+      "Editor"
+    ]
+  },
+  
+];
